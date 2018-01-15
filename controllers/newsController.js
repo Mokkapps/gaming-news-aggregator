3d2573c9a984066c7317fd8861fd830b792d6088
Michael Hoffmann <kontakt@mokkapps.de>
2018-01-15 19:57:19 +0100

    Sort news by descending date

--- controllers/newsController.js
+++ controllers/newsController.js
@@ -2,6 +2,12 @@ const mongoose = require('mongoose');
 const News = mongoose.model('News');
 
 exports.getNews = async (req, res) => {
-  const news = await News.find();
+  const news = await News.find({})
+    .sort({ date: -1 }) // Sort by descending date
+    .exec((err, docs) => {
+      if (err) {
+        console.error(`Error while getting news: ${err}`);
+      }
+    });
   return res.json(news);
 };
